d10159097cdda87a3a2a35ea90e93a16d67e7a91
Nazarii Havryliuk <ipz234_gnp@student.ztu.edu.ua>
2025-04-23 22:38:48 +0200

    Implemented task 3: Observer pattern

---
+++ Lab4/SupportSystem/ObserverPattern/LightHTML/Div.cs
@@ -0,0 +1,27 @@
+﻿namespace SupportSystem.ObserverPattern.LightHTML
+{
+    public class Div : HtmlElement
+    {
+        public string Content { get; set; }
+
+        public Div(string content = "") : base("div")
+        {
+            Content = content;
+        }
+
+        public void SimulateClick()
+        {
+            NotifyListeners("click");
+        }
+
+        public void SimulateMouseOver()
+        {
+            NotifyListeners("mouseover");
+        }
+
+        public override string Render()
+        {
+            return $"<div>{Content}</div>";
+        }
+    }
+}
