bda218b26508c74d147c32a19ca29b39d1a888eb
Nazarii Havryliuk <ipz234_gnp@student.ztu.edu.ua>
2025-05-16 21:07:20 +0200

    feat: розширено функціонал генерації QR-кодів та додано нові сервіси

---
+++ Lab6/Lab6/Services/PrintService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing.Printing;
+
+namespace Lab6.Services
+{
+    public class PrintService
+    {
+        public void PrintQRCode(string filePath)
+        {
+            PrintDocument printDoc = new PrintDocument();
+            printDoc.PrintPage += (sender, e) =>
+            {
+                try
+                {
+                    using (var image = System.Drawing.Image.FromFile(filePath))
+                    {
+                        e.Graphics.DrawImage(image, 10, 10, 300, 300);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error printing: {ex.Message}");
+                }
+            };
+            printDoc.Print();
+        }
+    }
+}
